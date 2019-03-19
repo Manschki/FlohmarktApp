7f3b4429464d45739be827875ceaed872af3c26d
Manuel  SEIFRIEDSBERGER <mseifriedsberger16@htl-grieskirchen.local>
2019-03-19 12:28:25 +0100

    nothing is working, after 4h

---
+++ app/src/main/java/com/example/mseifriedsberger16/flohmarktapp/Article.java
@@ -0,0 +1,92 @@
+package com.example.mseifriedsberger16.flohmarktapp;
+
+/**
+ * Created by mseifriedsberger16 on 19.03.2019.
+ */
+
+public class Article {
+    int id, price, phone;
+    String name, username, email, password;
+
+    public Article(int id, int price, int phone, String name, String username, String email, String password) {
+        this.id = id;
+        this.price = price;
+        this.phone = phone;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Article() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", price=" + price +
+                ", phone=" + phone +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/mseifriedsberger16/flohmarktapp/MainActivity.java
+++ app/src/main/java/com/example/mseifriedsberger16/flohmarktapp/MainActivity.java
@@ -1,7 +1,32 @@
 package com.example.mseifriedsberger16.flohmarktapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +34,131 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new MyAsyncTask(this).execute("get", "admin");
     }
+
+
+    /**
+     * Created by mseifriedsberger16 on 19.03.2019.
+     */
+
+    public class MyAsyncTask extends AsyncTask<String, Integer, String> {
+        private ListView listView;
+        private Context ctx;
+        private final String URL = "http://eaustria.no-ip.biz/flohmarkt/flohmarkt.php";
+
+        public MyAsyncTask(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // here we could do some UI manipulation before the worker
+            // thread starts
+            listView = findViewById(R.id.listView);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // do some UI manipulation while progress is modified
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //operation, username, name, price, password, email, phone, id
+
+            Log.d("TAG", "entered doInBackground");
+            String operation = strings[0];
+            String username = strings[1];
+            //Log.d("TAG", "url: "+URL+username+"/repos");
+            String sJson = "";
+            try {
+
+
+                /*connection.addRequestProperty("operation", operation);
+                connection.addRequestProperty("username", username);*/
+                switch (operation) {
+
+                    case "get":
+                        String finalUrl = URL + "?operation=" + operation + "&username=" + username;
+
+                        HttpURLConnection connection =
+                                (HttpURLConnection) new URL(finalUrl).openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setRequestProperty("Content-Type", "application/json");
+
+                        //connection.connect();
+                        int responseCode = connection.getResponseCode();
+                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                            BufferedReader reader = new BufferedReader(
+                                    new InputStreamReader(connection.getInputStream()));
+                            sJson = readResponseStream(reader);
+                        }
+                        //connection.disconnect();
+
+                        break;
+                    /*case "add":
+                        connection.addRequestProperty("name", strings[2]);
+                        connection.addRequestProperty("price", strings[3]);
+                        connection.addRequestProperty("password", strings[4]);
+                        connection.addRequestProperty("email", strings[5]);
+                        connection.addRequestProperty("phone", strings[6]);
+                        break;
+                    case "delete":
+                        connection.addRequestProperty("id", strings[7]);
+                        connection.addRequestProperty("password", strings[4]);
+                        break;*/
+                }
+
+            } catch (IOException e) {
+                Log.d("TAG", e.getLocalizedMessage());
+            }
+            return sJson;
+        }
+
+        private String readResponseStream(BufferedReader reader) throws IOException {
+            Log.d("TAG", "entered readResponseStreaulat");
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d("TAG", "entered onPostExecute");
+            // called after doInBackground finishes
+            Gson gson = new Gson();
+
+        TypeToken<List<String>> token = new TypeToken<List<String>>(){};
+
+//                JSONArray array = new JSONArray(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray arr = jsonObject.getJSONArray("data");
+
+                ArrayList<String> arrayList = gson.fromJson(arr, token);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+
+            //ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctx, android.R.layout.simple_list_item_1, arrayList);
+            listView = listView.findViewById(R.id.listView);
+            //listView.setAdapter(adapter);
+            super.onPostExecute(s);
+        }
+    }
+
 }
+
+
+
+
